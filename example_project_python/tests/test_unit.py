66cffbb412f644660f472f687527198a6cd962e8
ayshajamjam <aj2604@columbia.edu>
2023-04-09 22:16:50 -0400

    added unit test for top k words

--- example_project_python/tests/test_unit.py
+++ example_project_python/tests/test_unit.py
@@ -126,25 +126,25 @@ class Test(unittest.TestCase):
     #     self.assertEqual(c3, individual_word_count_non_stop_word(s3))
     #     self.assertEqual(c4, individual_word_count_non_stop_word(s4))
 
-    # def test_top_k_words(self):
-    #     s1 = "On offering to help the blind man, the man who then stole his car, had not, at that precise moment, had any evil intention, quite the contrary, what he did was nothing more than obey those feelings of generosity and altruism which, as everyone knows, are the two best traits of human nature and to be found in much more hardened criminals than this one, a simple car-thief without any hope of advancing in his profession, exploited by the real owners of this enterprise, for it is they who take advantage of the needs of the poor."
-    #     s2 = "My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges."
-    #     s3 = "The French are certainly misunderstood: — but whether the fault is theirs, in not sufficiently explaining themselves, or speaking with that exact limitation and precision which one would expect on a point of such importance, and which, moreover, is so likely to be contested by us — or whether the fault may not be altogether on our side, in not understanding their language always so critically as to know “what they would be at” — I shall not decide; but ‘tis evident to me, when they affirm, “That they who have seen Paris, have seen every thing,” they must mean to speak of those who have seen it by day-light."
-    #     s4 = "All I know is that I stood spellbound in his high-ceilinged studio room, with its north-facing windows in front of the heavy mahogany bureau at which Michael said he no longer worked because the room was so cold, even in midsummer; and that, while we talked of the difficulty of heating old houses, a strange feeling came upon me, as if it were not he who had abandoned that place of work but I, as if the spectacles cases, letters and writing materials that had evidently lain untouched for months in the soft north light had once been my spectacle cases, my letters and my writing materials."
+    def test_top_k_words(self):
+        s1 = "On offering to help the blind man, the man who then stole his car, had not, at that precise moment, had any evil intention, quite the contrary, what he did was nothing more than obey those feelings of generosity and altruism which, as everyone knows, are the two best traits of human nature and to be found in much more hardened criminals than this one, a simple car-thief without any hope of advancing in his profession, exploited by the real owners of this enterprise, for it is they who take advantage of the needs of the poor."
+        s2 = "My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges."
+        s3 = "The French are certainly misunderstood: — but whether the fault is theirs, in not sufficiently explaining themselves, or speaking with that exact limitation and precision which one would expect on a point of such importance, and which, moreover, is so likely to be contested by us — or whether the fault may not be altogether on our side, in not understanding their language always so critically as to know “what they would be at” — I shall not decide; but ‘tis evident to me, when they affirm, “That they who have seen Paris, have seen every thing,” they must mean to speak of those who have seen it by day-light."
+        s4 = "All I know is that I stood spellbound in his high-ceilinged studio room, with its north-facing windows in front of the heavy mahogany bureau at which Michael said he no longer worked because the room was so cold, even in midsummer; and that, while we talked of the difficulty of heating old houses, a strange feeling came upon me, as if it were not he who had abandoned that place of work but I, as if the spectacles cases, letters and writing materials that had evidently lain untouched for months in the soft north light had once been my spectacle cases, my letters and my writing materials."
 
-    #     e1 = "The the the THE THE"
-    #     e2 = "Scorpions lay in the sun, sizzling."
+        e1 = "The the the THE THE"
+        e2 = "Scorpions lay in the sun, sizzling."
 
-    #     self.assertEqual([('man', 2), ('car', 2), ('offering', 1)], top_k_words(s1, 3))
-    #     self.assertEqual(
-    #         [('warmth', 2), ('midges', 2), ('photogenic', 1), ('mother', 1), ('died', 1)], top_k_words(s2, 5)
-    #     )
-    #     self.assertEqual([('seen', 3), ('whether', 2)], top_k_words(s3, 2))
-    #     self.assertEqual([('room', 2), ('north', 2), ('cases', 2), ('letters', 2)], top_k_words(s4, 4))
+        self.assertEqual([('man', 2), ('car', 2), ('offering', 1)], top_k_words(s1, 3))
+        self.assertEqual(
+            [('warmth', 2), ('midges', 2), ('photogenic', 1), ('mother', 1), ('died', 1)], top_k_words(s2, 5)
+        )
+        self.assertEqual([('seen', 3), ('whether', 2)], top_k_words(s3, 2))
+        self.assertEqual([('room', 2), ('north', 2), ('cases', 2), ('letters', 2)], top_k_words(s4, 4))
 
-    #     self.assertRaises(ValueError, top_k_words, e1, 1)
-    #     self.assertRaises(ValueError, top_k_words, e1, 3)
-    #     self.assertRaises(ValueError, top_k_words, e2, 10)
+        self.assertRaises(ValueError, top_k_words, e1, 1)
+        self.assertRaises(ValueError, top_k_words, e1, 3)
+        self.assertRaises(ValueError, top_k_words, e2, 10)
 
 
 # if __name__ == '__main__':
