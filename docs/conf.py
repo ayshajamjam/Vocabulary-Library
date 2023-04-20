49a46753e20d155bb84a05794045590d9034672e
ayshajamjam <aj2604@columbia.edu>
2023-04-20 05:09:31 -0400

    hope: changed extension

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ master_doc="index"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark', 'sphinxcontrib.napoleon']
+extensions = ['recommonmark', 'sphinx.ext.napoleo']
 source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
